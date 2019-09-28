b92df98d7715d8b72ed442e7639a2c145263c7c0
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-09-28 10:22:15 +0800

    Create 函数.go

---
+++ 04函数/函数.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func add(x,y int) int{
+	return x+y
+}
+
+//多返回值
+func double(x,y int) (int,int){
+	return x,y
+}
+
+//命名返回值
+func circle(r int)(area,len float32){
+	area = float32(r) * float32(r) * 3.14
+	len = float32(r)*2*3.14
+	return   //自动把area和len填充到返回值中
+}
+func main(){
+	fmt.Println(add(10,20))  //30
+	//多返回值
+	x,y:=double(10,20)
+	fmt.Println(x,y)  //10 20
+	//命名返回值
+	myArea,myLen:=circle(2)
+	fmt.Println(myArea,myLen)  //12.56 12.56
+
+	//空白符
+	//比如说此时只需要获得面积，不需要获得周长，便可以用空白符来接收周长
+	_,len:=circle(2)
+	fmt.Println(len)  //12.56
+}
