ae2fa87d4b915cec8fe6e5455ea785cc9b9b088a
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-09-28 11:24:06 +0800

    Create ifelse.go

---
+++ 06 if-else/ifelse.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main(){
+	//if condition {
+	//} else if condition {
+	//} else {
+	//}
+
+	x:=10
+	if x<=0{
+		fmt.Println("less then 10")
+	}else if x<=5{
+		fmt.Println("less than 5")
+	}else{
+		fmt.Println("so big")
+	}
+	 //so big
+
+	 //if statement; condition {
+	//}
+
+	if a:=10+5;a<10{
+		fmt.Println("less than 10")
+	}else{
+		fmt.Println("so big") //so big
+	}
+}
