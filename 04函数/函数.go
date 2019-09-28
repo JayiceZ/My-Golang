70369d6e52f2181f14a83bba3a4fb451d510ec92
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-09-28 12:39:28 +0800

    Update 函数.go

--- 04函数/函数.go
+++ 04函数/函数.go
@@ -17,6 +17,14 @@ func circle(r int)(area,len float32){
 	len = float32(r)*2*3.14
 	return   //自动把area和len填充到返回值中
 }
+//多参数
+func addTotal(args...int) int{
+	s:=0
+	for num:=range args{
+		s+=num
+	}
+	return s
+}
 func main(){
 	fmt.Println(add(10,20))  //30
 	//多返回值
@@ -30,4 +38,7 @@ func main(){
 	//比如说此时只需要获得面积，不需要获得周长，便可以用空白符来接收周长
 	_,len:=circle(2)
 	fmt.Println(len)  //12.56
+	
+	fmt.Println(addTotal(1,2,3,4,5))
+	fmt.Println(1,2)
 }
